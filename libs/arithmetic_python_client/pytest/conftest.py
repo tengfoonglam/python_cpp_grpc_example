1751e35264157f174131201d70c5ce44dbf0e604
tengfoonglam <tengfoonglam@yahoo.com>
2022-08-26 00:25:00 +0200

    Prep conftest for max client

--- libs/arithmetic_python_client/pytest/conftest.py
+++ libs/arithmetic_python_client/pytest/conftest.py
@@ -1,8 +1,9 @@
+import logging
 import pytest
 import subprocess
 
 from threading import Event
-from typing import Generator, List, Tuple
+from typing import Callable, Generator, List, Optional, Tuple
 
 from arithmetic_python_client import AverageClient, MaxClient, PerformPrimeNumberDecompositionClient, SumClient
 
@@ -52,8 +53,8 @@ def configured_prime_client() -> Tuple[PerformPrimeNumberDecompositionClient, Li
 
 @pytest.fixture
 def open_configured_prime_client(
-    configured_prime_client: Tuple[PerformPrimeNumberDecompositionClient, List[int], List[bool]]
-) -> Generator[Tuple[PerformPrimeNumberDecompositionClient, List[int], List[bool]], None, None]:
+    configured_prime_client: Tuple[PerformPrimeNumberDecompositionClient, List[int], Event]
+) -> Generator[Tuple[PerformPrimeNumberDecompositionClient, List[int], Event], None, None]:
     client, output, decomposition_completed = configured_prime_client
     assert client.open() is True
     yield client, output, decomposition_completed
@@ -66,3 +67,42 @@ def open_sum_client() -> Generator[SumClient, None, None]:
     assert client.open() is True
     yield client
     assert client.close() is True
+
+
+@pytest.fixture
+def configured_max_client() -> Tuple[MaxClient, Event, Callable[[List[int], List[bool]], Generator[int, None, None]]]:
+
+    client = MaxClient()
+
+    expect_success_event = Event()
+    ready_to_send_next_number_event = Event()
+    expect_a_response_event = Event()
+    current_number: List[Optional[int]] = [None]
+
+    def input_generator(numbers_to_max: List[int], expect_a_response_list: List[bool]) -> Generator[int, None, None]:
+        TIMEOUT = 0.5
+        for number, expect_response in zip(numbers_to_max, expect_a_response_list):
+            ready_to_send_next_number_event.clear()
+            expect_a_response_event.clear()
+            current_number[0] = number
+            yield number
+            assert expect_response == expect_a_response_event.is_set()
+            if expect_response:
+                timed_out = ready_to_send_next_number_event.wait(TIMEOUT)
+                if timed_out:
+                    logging.error("Waiting to send next max number but timed out while waiting for a response")
+                    break
+
+    def on_receive(max: int) -> None:
+        expect_a_response_event.set()
+        assert max == current_number[0]
+        ready_to_send_next_number_event.set()
+
+    def on_completion(success: bool) -> None:
+        if success:
+            expect_success_event.set()
+
+    client.set_new_response_callback(callback=on_receive)
+    client.set_completed_callback(callback=on_completion)
+
+    return client, expect_success_event, input_generator
