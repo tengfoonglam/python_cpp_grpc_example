0971c6d9af5deaec12114e51f7a2b0c6e3ffd9da
tengfoonglam <tengfoonglam@yahoo.com>
2022-09-11 23:02:33 +0200

    fix docker check

--- scripts/install_grpc.sh
+++ scripts/install_grpc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # This variable will be empty if running in docker env, not otherwise
-RUNNING_IN_DOCKER=$(docker info > /dev/null 2>&1)
+NOT_RUNNING_IN_DOCKER=$(cat /proc/1/sched | head -n 1 | grep -E 'systemd|init')
 
 # Only run sudo if executed locally
 SUDO=''
-if [[ -n $RUNNING_IN_DOCKER ]]; then
-    echo "Running in Docker"
-else
+if [[ -n $NOT_RUNNING_IN_DOCKER ]]; then
     echo "Not running in Docker"
     SUDO='sudo'
+else
+	echo "Running in Docker"
 fi
 
 # Run with nproc-1 unless there is only one processor
@@ -20,11 +20,12 @@ fi
 echo "Building with $NPROC processor(s)"
 
 # Set environmental variables to install gRPC (if not in docker container, install locally in .local folder)
-if [[ -n $RUNNING_IN_DOCKER ]]; then
-    MY_INSTALL_DIR=/usr/local
-else
+if [[ -n $NOT_RUNNING_IN_DOCKER ]]; then
     MY_INSTALL_DIR=$HOME/.local
+else
+    MY_INSTALL_DIR=/usr/local
 fi
+
 echo "gRPC install directory: ${MY_INSTALL_DIR}"
 
 mkdir -p $MY_INSTALL_DIR
